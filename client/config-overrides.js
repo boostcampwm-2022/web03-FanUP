df103649d97aa640e358cd52c974ad58094a9e13
seongeuniii <kse8538@gmail.com>
2022-11-14 17:05:27 +0900

    Chore #6 : style 폴더 path alias 추가

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -5,6 +5,7 @@ const path = require('path');
 module.exports = override(
     addWebpackAlias({
         '@': path.resolve(__dirname, 'src'),
+        '@style': path.resolve(__dirname, 'src/style'),
         '@atoms': path.resolve(__dirname, 'src/components/UI/atoms'),
     })
 );
