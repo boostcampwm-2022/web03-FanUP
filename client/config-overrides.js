8acadeffa8dd69b6b4bc5bad39ee182fc097837f
seongeuniii <kse8538@gmail.com>
2022-11-16 00:21:18 +0900

    Chore #4 : path alias organisms 추가

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -6,6 +6,7 @@ module.exports = override(
     addWebpackAlias({
         '@': path.resolve(__dirname, 'src'),
         '@atoms': path.resolve(__dirname, 'src/components/UI/atoms'),
+        '@organisms': path.resolve(__dirname, 'src/components/UI/organisms'),
         '@images': path.resolve(__dirname, 'src/images'),
         '@pages': path.resolve(__dirname, 'src/pages'),
         '@style': path.resolve(__dirname, 'src/style'),
