ae153d65b9df77160dbd8a6059b3709ac2e8849a
seongeuniii <kse8538@gmail.com>
2022-11-15 13:48:37 +0900

    Chore #4 : path alias images 추가

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -5,8 +5,9 @@ const path = require('path');
 module.exports = override(
     addWebpackAlias({
         '@': path.resolve(__dirname, 'src'),
+        '@atoms': path.resolve(__dirname, 'src/components/UI/atoms'),
+        '@images': path.resolve(__dirname, 'src/images'),
         '@pages': path.resolve(__dirname, 'src/pages'),
         '@style': path.resolve(__dirname, 'src/style'),
-        '@atoms': path.resolve(__dirname, 'src/components/UI/atoms'),
     })
 );
