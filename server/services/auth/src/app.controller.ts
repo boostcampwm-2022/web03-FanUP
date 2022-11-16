181c170cfc05fb84e64a3e64be860e5f22f5aaeb
Sophoca <tjdqls1668@naver.com>
2022-11-16 23:46:35 +0900

    Fix: rollback template code

--- server/services/auth/src/app.controller.ts
+++ server/services/auth/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,10 +8,5 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-
-  @Post('google')
-  googleLogin(@Body() { accessCode }: Record<'accessCode', string>) {
-    return this.appService.googleLogin(accessCode);
   }
 }
--- server/services/auth/src/app.module.ts
+++ server/services/auth/src/app.module.ts
@@ -1,26 +1,11 @@
 import { Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { HttpModule } from '@nestjs/axios';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { JwtStrategy } from './strategies/jwt.strategy';
-import { JwtRefreshStrategy } from './strategies/refresh.strategy';
-import { PrismaService } from './prisma.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [
-    JwtModule.registerAsync({
-      useFactory: () => ({
-        secret: 'secret', // TODO: env 처리
-        signOptions: {
-          expiresIn: '1h',
-        },
-      }),
-    }),
-    HttpModule,
-  ],
+  imports: [AuthModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService, JwtStrategy, JwtRefreshStrategy],
+  providers: [AppService],
 })
 export class AppModule {}
