10651ff6e4ea30231294c3057f504f8efc7d1633
Sophoca <tjdqls1668@naver.com>
2022-11-16 14:58:50 +0900

    feat: google oauth login, jwt service

--- server/services/auth/src/app.module.ts
+++ server/services/auth/src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { HttpModule } from '@nestjs/axios';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { JwtStrategy } from './strategies/jwt.strategy';
+import { JwtRefreshStrategy } from './strategies/refresh.strategy';
+import { PrismaService } from './prisma.service';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: () => ({
+        secret: 'secret', // TODO: env 처리
+        signOptions: {
+          expiresIn: '1h',
+        },
+      }),
+    }),
+    HttpModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService, JwtStrategy, JwtRefreshStrategy],
 })
 export class AppModule {}
---
+++ server/services/auth/src/strategies/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+
+import { PrismaService } from 'src/prisma.service';
+
+interface JwtPayload {
+  userId: number;
+}
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private prisma: PrismaService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: '1234', // TODO: env 처리
+    });
+  }
+
+  async validate({ userId }: JwtPayload) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: userId },
+    });
+    return user;
+  }
+}
