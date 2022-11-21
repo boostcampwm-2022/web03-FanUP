95030e66600d8f0e4cef143a63d07c682c9f24f4
seongeuniii <kse8538@gmail.com>
2022-11-21 15:11:17 +0900

    Feat : ChatList 컴포넌트 구현

---
+++ client/src/components/UI/molecules/ChatList/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Chat from '@atoms/FanUP/chat';
+
+const StyledChatList = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    margin-bottom: 50px;
+    max-height: 90%;
+    overflow: auto;
+    padding-top: 5px;
+    &::-webkit-scrollbar {
+        display: none;
+    }
+`;
+
+const ChatList = ({ chatData }: any) => {
+    return (
+        <StyledChatList>
+            {chatData.map(({ nickname, isArtist, content }: any, idx: number) => (
+                <Chat
+                    key={content + idx}
+                    isArtist={isArtist}
+                    nickname={nickname}
+                    content={content}
+                />
+            ))}
+        </StyledChatList>
+    );
+};
+
+export default ChatList;
