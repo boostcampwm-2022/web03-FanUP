40926e1da8a568cea846d3584d6358147e128e69
seongeuniii <kse8538@gmail.com>
2022-11-21 15:10:38 +0900

    Chore : 네이밍 변경 - ChatContainer -> ChatContainer

--- client/src/components/UI/organisms/ChatContainer/index.tsx
+++ client/src/components/UI/organisms/ChatContainer/index.tsx
@@ -1,9 +1,10 @@
-import Chat from '@/components/UI/atoms/FanUP/chat';
-import ChatForm from '@/components/UI/atoms/FanUP/chatForm';
 import React, { useState } from 'react';
 import styled from 'styled-components';
 
-const dummyChats = [
+import InputForm from '@molecules/InputForm';
+import ChatList from '@molecules/ChatList';
+
+const dummyChatData = [
     { nickname: '장원영', isArtist: true, content: '안녕하세요 ㅎㅎ' },
     { nickname: '성은', isArtist: false, content: '예쁘다~' },
     {
@@ -27,34 +28,13 @@ const ChatBoxWrapper = styled.div`
     height: 100%;
 `;
 
-const ChatsWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 10px;
-    margin-bottom: 50px;
-    max-height: 90%;
-    overflow: auto;
-    padding-top: 5px;
-    &::-webkit-scrollbar {
-        display: none;
-    }
-`;
-
 const ChatBox = () => {
-    const [chats, setChats] = useState(dummyChats);
+    const [chatData, setChatData] = useState(dummyChatData);
+
     return (
         <ChatBoxWrapper data-testid="chatBox">
-            <ChatsWrapper>
-                {chats.map(({ nickname, isArtist, content }, idx) => (
-                    <Chat
-                        key={content + idx}
-                        isArtist={isArtist}
-                        nickname={nickname}
-                        content={content}
-                    />
-                ))}
-            </ChatsWrapper>
-            <ChatForm />
+            <ChatList chatData={chatData} />
+            <InputForm />
         </ChatBoxWrapper>
     );
 };
--- client/src/components/UI/organisms/ChatContainer/test/index.test.tsx
+++ client/src/components/UI/organisms/ChatContainer/test/index.test.tsx
@@ -1,10 +1,11 @@
+import React from 'react';
 import { renderWithContext } from '@/utils/test/renderWithContext';
 import { screen } from '@testing-library/react';
-import ChatBox from '..';
+import ChatContainer from '..';
 
 describe('<ChatBox />', () => {
     it('rendering test', () => {
-        renderWithContext(<ChatBox />);
-        expect(screen.getByTestId('chatBox')).toBeInTheDocument();
+        renderWithContext(<ChatContainer />);
+        expect(screen.getByTestId('chatContainer')).toBeInTheDocument();
     });
 });
