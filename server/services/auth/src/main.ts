23137623b36100d4f3118bffd669ae3a88de185b
Sophoca <tjdqls1668@naver.com>
2022-11-23 13:40:51 +0900

    Fix: Auth service를 hybrid service로 전환

--- server/services/auth/src/main.ts
+++ server/services/auth/src/main.ts
@@ -3,17 +3,18 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 import { AppModule } from './app.module';
 
+const HYBRID_AUTH_PORT = 4001;
+
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    {
-      transport: Transport.TCP,
-      options: {
-        port: 3001,
-      },
+  const app = await NestFactory.create(AppModule);
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.TCP,
+    options: {
+      port: 3001,
     },
-  );
-  await app.listen();
-  console.log('Auth service is running');
+  });
+  await app.startAllMicroservices();
+  await app.listen(HYBRID_AUTH_PORT);
+  console.log(`Auth service is running on port: ${await app.getUrl()}`);
 }
 bootstrap();
