bf95c10f5a70691dbf73068f492833411fe950f2
Sophoca <tjdqls1668@naver.com>
2022-11-23 14:05:44 +0900

    Feat: Google Strategy 작성

---
+++ server/services/auth/src/auth/strategies/google.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, Profile } from 'passport-google-oauth20';
+
+@Injectable()
+export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
+  constructor(readonly config: ConfigService) {
+    super({
+      clientID: config.get('GOOGLE_CLIENT_ID'),
+      clientSecret: config.get('GOOGLE_CLIENT_SECRET'),
+      callbackURL: config.get('GOOGLE_CALLBACK_URL'),
+      scope: ['email', 'profile'],
+    });
+  }
+
+  validate(accessToken: string, refreshToken: string, profile: Profile) {
+    const { id, name, emails } = profile;
+
+    return {
+      provider: 'google',
+      providerId: id,
+      name: name.givenName,
+      email: emails[0].value,
+    };
+  }
+}
