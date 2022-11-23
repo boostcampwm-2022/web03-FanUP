758bb169a86812ead5048f915340282282211d0b
CEOJINSUNG <jinsung1048@gmail.com>
2022-11-23 14:41:18 +0900

    Fix : 에러 수정

--- server/gateway/src/main.ts
+++ server/gateway/src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.enableCors();
   console.log('Gateway is running on: ' + (await app.getUrl()));
 }
 bootstrap();
