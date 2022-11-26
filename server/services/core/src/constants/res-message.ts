aac209d2caa508bd8f0aec79e0ebfbfc310606b5
CEOJINSUNG <jinsung1048@gmail.com>
2022-11-27 01:09:26 +0900

    Feat/#49 : Response 설정

---
+++ server/services/core/src/constants/res-message.ts
@@ -0,0 +1,6 @@
+const ResMessage = {
+  // 채팅 관련
+  CHAT_ROOM_NOT_FOUND: '채팅방이 존재하지 않습니다.',
+};
+
+export { ResMessage };
---
+++ server/services/core/src/constants/res-status-code.ts
@@ -0,0 +1,12 @@
+const ResStatusCode = {
+  OK: 200,
+  CREATED: 201,
+  ACCEPTED: 202,
+
+  BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
+  FORBIDDEN: 403,
+  NOT_FOUND: 404,
+};
+
+export { ResStatusCode };
