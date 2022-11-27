357e63e543dfded86b50b3cfdd6e0e574c22a335
CEOJINSUNG <jinsung1048@gmail.com>
2022-11-27 17:07:18 +0900

    Feat/#49 : 채팅 관련 응답 메시지 작성

--- server/services/core/src/common/constants/res-message.ts
+++ server/services/core/src/common/constants/res-message.ts
@@ -3,6 +3,10 @@ const ResMessage = {
   CORE_HELLO: 'CORE 서비스입니다.',
 
   // 채팅 관련
+  CREATE_CHAT: '해당 채팅 메시지를 생성하였습니다.',
+  GET_ALL_CHAT: '해당 채팅 방의 채팅 내역을 가져왔습니다.',
+
+  CHAT_NOT_FOUND: '채팅 내역이 존재하지 않습니다.',
   CHAT_ROOM_NOT_FOUND: '채팅방이 존재하지 않습니다.',
 };
 
--- server/services/core/src/domain/chat/service/chat.service.ts
+++ server/services/core/src/domain/chat/service/chat.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { RpcException } from '@nestjs/microservices';
+import { Chat, Prisma } from '@prisma/client';
+import {
+  ChatNotFoundException,
+  ChatRoomNotFoundException,
+} from '../../../common/exception';
 import { PrismaService } from '../../../provider/prisma/prisma.service';
-import { CreateChatDto } from '../dto/create-chat.dto';
-import { UpdateChatDto } from '../dto/update-chat.dto';
+import { ChatDto, CreateChatDto } from '../dto';
 
 @Injectable()
 export class ChatService {
@@ -14,7 +18,27 @@ export class ChatService {
     return await this.prisma.chat.create({ data: createChatDto });
   }
 
-  async findChatByFanUPId(fanupId: number) {
-    return await this.prisma.chat.findMany({ where: { fanup_id: fanupId } });
+  // 특정 방의 채팅 메시지 전체를 가져오는 함수
+  async findChatByFanUPId(fanupId: number): Promise<ChatDto[]> {
+    let chatList: ChatDto[];
+    try {
+      chatList = await this.prisma.chat.findMany({
+        where: { fanup_id: fanupId },
+        select: {
+          email: true,
+          is_artist: true,
+          message: true,
+          created_at: true,
+        },
+      });
+
+      if (chatList.length === 0) {
+        throw new ChatNotFoundException();
+      }
+    } catch (err) {
+      console.log(err);
+      return err;
+    }
+    return chatList;
   }
 }
