92ba13b60de577e66db05e38c948df46a6573aee
CEOJINSUNG <jinsung1048@gmail.com>
2022-11-28 21:56:44 +0900

    Feat/#49 : 프리즈마 스키마 생성

--- server/services/core/src/domain/fanup/controller/fanup.controller.ts
+++ server/services/core/src/domain/fanup/controller/fanup.controller.ts
@@ -7,29 +7,4 @@ import { UpdateFanupDto } from '../dto/update-fanup.dto';
 @Controller()
 export class FanupController {
   constructor(private readonly fanupService: FanupService) {}
-
-  @MessagePattern('createFanup')
-  create(@Payload() createFanupDto: CreateFanupDto) {
-    return this.fanupService.create(createFanupDto);
-  }
-
-  @MessagePattern('findAllFanup')
-  findAll() {
-    return this.fanupService.findAll();
-  }
-
-  @MessagePattern('findOneFanup')
-  findOne(@Payload() id: number) {
-    return this.fanupService.findOne(id);
-  }
-
-  @MessagePattern('updateFanup')
-  update(@Payload() updateFanupDto: UpdateFanupDto) {
-    return this.fanupService.update(updateFanupDto.id, updateFanupDto);
-  }
-
-  @MessagePattern('removeFanup')
-  remove(@Payload() id: number) {
-    return this.fanupService.remove(id);
-  }
 }
