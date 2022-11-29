4cce25bb324dc7f4aa26bf379fcd4ce3b723db48
CEOJINSUNG <jinsung1048@gmail.com>
2022-11-29 23:42:56 +0900

    Feat/#66 : 크론잡을 등록하고 제거하는 기능 생성

--- server/services/core/src/job/task/basic.task.ts
+++ server/services/core/src/job/task/basic.task.ts
@@ -1,5 +1,6 @@
 import { Logger } from '@nestjs/common';
 import { Cron, SchedulerRegistry } from '@nestjs/schedule';
+import { CronJob } from 'cron';
 
 export class BasicTask {
   private readonly logger = new Logger(BasicTask.name);
@@ -10,5 +11,17 @@ export class BasicTask {
   @Cron('0 0 0 * * *', { name: 'registerTask' })
   registerTask() {
     this.logger.log('매일 밤 12시에 실행되는 크론잡');
+  }
+
+  addCronJob(name: string, task: Function) {
+    const job = new CronJob('* * * * * *', () => task);
+    this.schedulerRegistry.addCronJob(name, job);
+    this.logger.log(`job ${name} added!`);
+  }
+
+  stopCronJob(name: string) {
+    const job = this.schedulerRegistry.getCronJob(name);
+    job.stop();
+    this.logger.log(`job ${name} deleted`);
   }
 }
