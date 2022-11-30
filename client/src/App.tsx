4e915d6dd5fdd5eb9675cb934ba634f9b7d0ba11
By_juun <neostgeart@gmail.com>
2022-11-30 13:32:48 +0900

    Test nvm link

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import GlobalStyle from '@style/GlobalStyle';
 import { FanUP, Home, Login, NotFound, AuthCallback, Tickets, Ticket } from './Routes';
 
 function App() {
-    console.log('github actions t esT ');
+    console.log('github actions t esdT ');
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
