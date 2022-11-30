97e3c98167d3191d0f842373b810ad5d71868192
By_juun <neostgeart@gmail.com>
2022-11-30 13:16:46 +0900

    Test

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import GlobalStyle from '@style/GlobalStyle';
 import { FanUP, Home, Login, NotFound, AuthCallback, Tickets, Ticket } from './Routes';
 
 function App() {
-    console.log('github actions tes1!');
+    console.log('github actions tes2');
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
