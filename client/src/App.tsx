58c7ce1dfce3ee38755bbf3b3aeec1a608f19f7d
By_juun <neostgeart@gmail.com>
2022-11-30 13:22:03 +0900

    Test Sudo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,7 +7,7 @@ import GlobalStyle from '@style/GlobalStyle';
 import { FanUP, Home, Login, NotFound, AuthCallback, Tickets, Ticket } from './Routes';
 
 function App() {
-    console.log('github actions tesT2');
+    console.log('github actions tesT ');
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
