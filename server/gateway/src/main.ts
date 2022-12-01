77d7137b28dc4e2b5fe70f960a0fb579f9a8641e
CEOJINSUNG <jinsung1048@gmail.com>
2022-12-01 14:21:36 +0900

    Fix : host 연결 에러 해결

--- server/gateway/src/main.ts
+++ server/gateway/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: true,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    methods: '*',
     credentials: true,
   });
 
