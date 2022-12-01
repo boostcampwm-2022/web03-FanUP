3a3b63f9e60a2361279d5d92914ada2ca1242562
CEOJINSUNG <jinsung1048@gmail.com>
2022-12-01 15:12:03 +0900

    Fix : origin 설정 변경

--- server/gateway/src/main.ts
+++ server/gateway/src/main.ts
@@ -5,8 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: true,
-    credentials: true,
+    origin: '*',
   });
 
   app.useGlobalPipes(
