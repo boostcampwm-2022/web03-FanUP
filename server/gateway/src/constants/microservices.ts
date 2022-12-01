0e05af26e69de623773b96465ce49171fb801a85
Sophoca <tjdqls1668@naver.com>
2022-12-01 13:42:35 +0900

    Hotfix: gateway host 수정

--- server/gateway/src/constants/microservices.ts
+++ server/gateway/src/constants/microservices.ts
@@ -2,17 +2,17 @@ const environment = process.env.NODE_ENV || 'development';
 const MICRO_SERVICES = {
   AUTH: {
     NAME: 'AUTH_SERVICE',
-    HOST: environment === 'development' ? 'localhost' : '127.0.0.1',
+    HOST: environment === 'development' ? 'localhost' : 'localhost', // todo: 배포시에는 서버의 IP로 변경
     PORT: 3001,
   },
   CORE: {
     NAME: 'CORE_SERVICE',
-    HOST: environment === 'development' ? 'localhost' : '127.0.0.1',
+    HOST: environment === 'development' ? 'localhost' : 'localhost',
     PORT: 3002,
   },
   TICKET: {
     NAME: 'TICKET_SERVICE',
-    HOST: environment === 'development' ? 'localhost' : '127.0.0.1',
+    HOST: environment === 'development' ? 'localhost' : 'localhost',
     PORT: 3003,
   },
 };
