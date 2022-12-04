e007282475167a830bdb64f78cfe08eebee83a41
Sophoca <tjdqls1668@naver.com>
2022-12-05 04:14:30 +0900

    Refactor: ticket service 이동

---
+++ server/services/ticket/src/domain/ticket/dto/create-ticket.dto.ts
@@ -0,0 +1,37 @@
+import { Type } from 'class-transformer';
+import { IsDateString, IsNumber, IsString } from 'class-validator';
+
+class CreateTicketDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  content: string;
+
+  @IsNumber()
+  artistId: number;
+
+  @IsDateString()
+  salesTime: Date;
+
+  @IsDateString()
+  startTime: Date;
+
+  @IsNumber()
+  @Type(() => Number)
+  totalAmount: number;
+
+  @IsNumber()
+  @Type(() => Number)
+  numberTeam: number;
+
+  @IsNumber()
+  @Type(() => Number)
+  timeTeam: number;
+
+  @IsNumber()
+  @Type(() => Number)
+  price: number;
+}
+
+export default CreateTicketDto;
---
+++ server/services/ticket/src/domain/ticket/dto/update-ticket.dto.ts
@@ -0,0 +1,6 @@
+import { PartialType } from '@nestjs/mapped-types';
+import CreateTicketDto from './create-ticket.dto';
+
+class UpdateTicketDto extends PartialType(CreateTicketDto) {}
+
+export default UpdateTicketDto;
