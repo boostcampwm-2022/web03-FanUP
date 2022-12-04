d3cb230750386c4f77fa4151ab4669aa8b784486
Sophoca <tjdqls1668@naver.com>
2022-12-05 04:38:04 +0900

    Refactor: ticket service 이동

--- server/services/ticket/src/ticket/dto/update-ticket.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import CreateTicketDto from './create-ticket.dto';
-
-class UpdateTicketDto extends PartialType(CreateTicketDto) {}
-
-export default UpdateTicketDto;
