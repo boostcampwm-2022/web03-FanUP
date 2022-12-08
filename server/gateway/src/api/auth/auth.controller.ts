8ef52235034caf0935c85f4b2f05ec209827dbbd
Sophoca <tjdqls1668@naver.com>
2022-12-09 05:11:55 +0900

    Feat: Global exception filter 적용

--- server/gateway/src/api/auth/auth.controller.ts
+++ server/gateway/src/api/auth/auth.controller.ts
@@ -1,8 +1,19 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  UseFilters,
+  UseGuards,
+} from '@nestjs/common';
+import { AllGlobalExceptionsFilter } from 'src/common/exception/filter/global-exception.filter';
 import { JwtAuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
+import CreateArtistDto from './dto/create-artist.dto';
 import LoginDto from './dto/login.dto';
 
+@UseFilters(AllGlobalExceptionsFilter)
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
@@ -19,7 +30,16 @@ export class AuthController {
 
   @Get('/me')
   @UseGuards(JwtAuthGuard)
-  getUserInfo(@Req() req) {
-    return this.authService.getUserInfo(req.user.id);
+  getUserInfo(@Req() { user }) {
+    return this.authService.getUserInfo(user.id);
+  }
+
+  @Post('/artist')
+  @UseGuards(JwtAuthGuard)
+  async createArtist(@Req() { user }, @Body() body: CreateArtistDto) {
+    return this.authService.createArtist({
+      userId: user.id,
+      ...body,
+    });
   }
 }
