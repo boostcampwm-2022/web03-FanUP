b38372633495b2b6ef96583083e436177653b737
CEOJINSUNG <jinsung1048@gmail.com>
2022-12-11 21:20:56 +0900

    Fix/#135 : 로컬 환경 변경

--- server/services/core/src/main.ts
+++ server/services/core/src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
     {
       transport: Transport.TCP,
       options: {
-        host: '0.0.0.0',
+        host: process.env.NODE_ENV === 'development' ? 'localhost' : '0.0.0.0',
         port: 3002,
       },
     },
