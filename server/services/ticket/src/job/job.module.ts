2ae958944fb8bb9a23fbecb0b94ca9e450309044
CEOJINSUNG <jinsung1048@gmail.com>
2022-12-13 00:06:27 +0900

    Fix : 의존성 에러

--- server/services/ticket/src/job/job.module.ts
+++ server/services/ticket/src/job/job.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { MICRO_SERVICES } from 'src/common/constants/microservices';
 import { UserTicketService } from 'src/domain/user-ticket/user-ticket.service';
+import { PrismaService } from 'src/provider/prisma/prisma.service';
 import { JobListener } from './job.listener';
 
 @Module({
@@ -25,6 +26,6 @@ import { JobListener } from './job.listener';
       },
     ]),
   ],
-  providers: [JobListener, UserTicketService],
+  providers: [JobListener, UserTicketService, PrismaService],
 })
 export class JobModule {}
