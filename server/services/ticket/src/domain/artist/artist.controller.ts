87735a7ce5e02281e982d258a1de11ec78877d40
SungBeen Hong <tjdqls1668@naver.com>
2022-12-12 22:32:38 +0900

    Feat: create artist logic 추가

--- server/services/ticket/src/domain/artist/artist.controller.ts
+++ server/services/ticket/src/domain/artist/artist.controller.ts
@@ -1,7 +1,17 @@
 import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { Artist } from '@prisma/client';
 import { ArtistService } from './artist.service';
 
 @Controller()
 export class ArtistController {
   constructor(private readonly artistService: ArtistService) {}
+
+  @MessagePattern({ cmd: 'createArtist' })
+  async createArtist(
+    @Payload() requestCreateArtistDto: Artist,
+  ): Promise<Artist> {
+    console.log('create artist on ticket service');
+    return this.artistService.create(requestCreateArtistDto);
+  }
 }
--- server/services/ticket/src/domain/artist/artist.service.ts
+++ server/services/ticket/src/domain/artist/artist.service.ts
@@ -1,7 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { Artist } from '@prisma/client';
 import { PrismaService } from 'src/provider/prisma/prisma.service';
 
 @Injectable()
 export class ArtistService {
   constructor(private readonly prisma: PrismaService) {}
+
+  create(createArtistDto: Artist) {
+    return this.prisma.artist.create({
+      data: createArtistDto,
+    });
+  }
 }
