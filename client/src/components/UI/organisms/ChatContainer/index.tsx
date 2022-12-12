2e40d02f16083106ae67deed215e6a66fca88026
seongeuniii <kse8538@gmail.com>
2022-12-12 15:17:53 +0900

    Feat : 채팅 데이터 포맷 변경

--- client/src/components/UI/organisms/ChatContainer/index.tsx
+++ client/src/components/UI/organisms/ChatContainer/index.tsx
@@ -2,14 +2,17 @@ import React, { FC, useEffect, useState, useCallback } from 'react';
 import styled from 'styled-components';
 
 import { socket, SOCKET_EVENTS, connectSocket } from '@/socket';
+import { MyEmail, MyNickName } from '@utils/generateRandomString';
 import InputForm from '@molecules/InputForm';
 import ChatList from '@molecules/ChatList';
-import { MyEmail, MyNickName } from '@/utils/generateRandomString';
+import { useParams } from 'react-router-dom';
+import { useGetUserQuery } from '@services/user.service';
+import { IUser } from '@/types/user';
 
 export interface ChatMessage {
     date: number;
     isArtist: boolean;
-    nickname: string;
+    nickname?: string;
     message: string;
 }
 
@@ -19,38 +22,42 @@ const StyledChatContainer = styled.div`
 `;
 
 const ChatContainer: FC = () => {
-    const [room, setRoom] = useState<string>('1');
+    const { fanUpId } = useParams();
+    // TODO : undefined 에러 처리
+    const userData = useGetUserQuery()?.data;
+    const [userId, setUserId] = useState<number | undefined>(userData?.id);
+    const [userNickname, setuserNickname] = useState<string | undefined>(userData?.nickname);
+    const [room, setRoom] = useState<string | undefined>(fanUpId);
     const [message, setMessage] = useState('');
     const [chatData, setChatData] = useState<ChatMessage[]>([]);
 
-    // useEffect(() => {
-    //     socket?.emit(SOCKET_EVENTS.requestChat, { room: room });
-    //     socket?.on(SOCKET_EVENTS.responseChat, (data) => setChatData(() => [...data.result]));
-    //     socket?.on(SOCKET_EVENTS.receiveMessage, (data) => setChatData((curr) => [...curr, data]));
+    useEffect(() => {
+        if (!socket || !room || !userId) return;
 
-    //     socket?.emit(SOCKET_EVENTS.requestParticipantUser, { room: room });
-    //     socket?.on(SOCKET_EVENTS.responseParticipantUser, (data) => console.log(data));
-    // }, []);
+        socket.emit(SOCKET_EVENTS.requestChat, { room: room });
+        socket.on(SOCKET_EVENTS.responseChat, (data) =>
+            setChatData((curr) => [...curr, ...data.result])
+        );
+        socket.on(SOCKET_EVENTS.receiveMessage, (data) => setChatData((curr) => [...curr, data]));
+        socket.on(SOCKET_EVENTS.failSendMessage, () => alert('메시지 전송에 실패하였습니다.'));
+        socket.emit(SOCKET_EVENTS.requestParticipantUser, { room: room });
+        socket.on(SOCKET_EVENTS.responseParticipantUser, (data) => console.log(data));
+    }, [socket]);
 
     const handleSubmit = useCallback(
         (e: React.FormEvent<HTMLButtonElement>) => {
             e.preventDefault();
-            console.log('메시지 전송');
-
-            // MyEmail, MyNickName은 랜덤스트링 생성기로 생성한 랜덤한 문자열 값임.
-            // TODO : 값 가져오기
             const data = {
-                email: MyEmail,
-                nickname: MyNickName,
-                room: '1',
+                userId: userId,
+                nickname: userNickname,
+                room: room,
                 isArtist: false,
                 message: message,
             };
-
             socket?.emit(SOCKET_EVENTS.sendMessage, data);
             setMessage('');
         },
-        [message]
+        [message, room]
     );
 
     return (
