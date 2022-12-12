4caf8d9798edd610c14e4c421fe61db8faf9d777
SungBeen Hong <tjdqls1668@naver.com>
2022-12-12 15:29:17 +0900

    Feat: user nickname update api 추가, user controller - auth module 연동

--- server/services/auth/src/app.module.ts
+++ server/services/auth/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ArtistModule } from './domain/artist/artist.module';
 import { AuthModule } from './domain/auth/auth.module';
 import { FavoriteModule } from './domain/favorite/favorite.module';
+import { UserModule } from './domain/user/user.module';
 
 @Module({
-  imports: [AuthModule, ArtistModule, FavoriteModule],
+  imports: [AuthModule, ArtistModule, FavoriteModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
