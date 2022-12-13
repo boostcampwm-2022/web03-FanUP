7d4b3f6ea5d990ddb11f44ad8723fa6f4e0e58df
By_juun <neostgeart@gmail.com>
2022-12-13 21:42:25 +0900

    Fix: Howling

--- client/src/components/UI/atoms/Video/index.tsx
+++ client/src/components/UI/atoms/Video/index.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect, useRef } from 'react';
 
-const Video = ({ stream }: { stream: any }) => {
+const Video = ({ stream, isMyVideo }: { stream: any; isMyVideo: boolean }) => {
     const videoRef = useRef<HTMLVideoElement>(null);
     useEffect(() => {
         if (!videoRef.current) return;
         if (!stream) return;
         videoRef.current.srcObject = stream;
     }, [stream]);
-    return <video ref={videoRef} width="100%" height="100%" autoPlay playsInline muted />;
+    return (
+        <video ref={videoRef} width="100%" height="100%" autoPlay playsInline muted={isMyVideo} />
+    );
 };
 
 export default Video;
