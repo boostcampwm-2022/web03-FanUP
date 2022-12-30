0ac7d1dcc2d5aaf6cae990b53233c295cdd2d563
CEOJINSUNG <jinsung1048@gmail.com>
2022-12-30 21:46:21 +0900

    Test/#212 : create test 완성

--- server/services/core/src/domain/notification/service/notification.service.spec.ts
+++ server/services/core/src/domain/notification/service/notification.service.spec.ts
@@ -1,21 +1,74 @@
+/**
+ * @jest-environment node
+ */
+
+import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { ResMessage } from '../../../common/constants';
+import { NotificationCreateException } from '../../../common/exception';
 import { PrismaService } from '../../../provider/prisma/prisma.service';
 import { NotificationModule } from '../notification.module';
 import { NotificationService } from './notification.service';
 
-describe('NotificationService', () => {
-  let service: NotificationService;
+jest.mock('../../../provider/prisma/prisma.service', () => ({
+  PrismaService: jest.fn().mockImplementation(() => ({
+    notification: {
+      create: jest.fn(),
+      findMany: jest.fn(),
+      findFirst: jest.fn(),
+      updateMany: jest.fn(),
+    },
+  })),
+}));
 
-  beforeEach(async () => {
+describe('NotificationService', () => {
+  let app: INestApplication;
+  let service: NotificationService;
+  let prisma: PrismaService;
+
+  const notification = {
+    type: 'type',
+    info: 'info',
+    user_id: 1,
+    message: 'message',
+    read: false,
+  };
+
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [NotificationModule],
       providers: [NotificationService, PrismaService],
     }).compile();
 
+    app = module.createNestApplication();
+    await app.init();
+
     service = module.get<NotificationService>(NotificationService);
+    prisma = module.get<PrismaService>(PrismaService);
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('create 성공 테스트', async () => {
+    prisma.notification.create = jest.fn().mockResolvedValue(notification);
+    expect(await service.create(notification)).toEqual(notification);
+  });
+
+  it('create 실패 테스트', async () => {
+    try {
+      prisma.notification.create = jest.fn().mockImplementation(() => {
+        throw new Error();
+      });
+      await service.create(notification);
+    } catch (err) {
+      expect(err).toBeInstanceOf(NotificationCreateException);
+      expect(err.message).toBe(ResMessage.NOTIFICATION_BAD_REQUEST);
+    }
   });
 });
